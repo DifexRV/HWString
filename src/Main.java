781c0dea3216c51ff3a9d84a623f9d7cc2c6ad6d
DifexRV <roOmkin@gmail.com>
2022-10-23 21:24:41 +0700

    ДЗ на проверку - Строки.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+
+    public static void task1()    {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2()    {
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    public static void task3()    {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё","е");
+        fullName = fullName.replace("Ё","Е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+    }
+
 }
